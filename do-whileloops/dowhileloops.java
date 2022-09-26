427a47c5a14b50b3b214a69dd4374a6acb891976
Drew Miller <drew.miller@mrjaffesclass.com>
2022-09-26 11:11:22 -0700

    dowhileloops

--- do-whileloops/dowhileloops.java
+++ do-whileloops/dowhileloops.java
@@ -6,24 +6,24 @@ public class dowhileloops
 
     public void run() {
         int i = 0;
-        while (i < 15) {
+        do {
             System.out.println("i is " + i + " but is less than 15");
             i++;
-        }
+        } while (i < 15);
 
         int k = 10;
-        while (k > 5) {
+        do {
             System.out.println("k is " + k + " but is greater than 5");
             k--;
-        }
+        } while (k > 5);
 
         int a = 5;
         int b = 12;
-        while (a < 10 & b <= 20) {
+        do {
             System.out.println("a is " + a + " but is less than 10," + "b is " + b + " but is less than or equal to 20");
             b++;
             a++;
-        }
+        } while (a < 10 & b <= 20);
     }
 
     public static void main(String[] args)
